e5217709c625b3cfc3579cecfef4d753cd08c036
Fu Jun <fulin.fujun@outlook.es>
2025-11-18 19:45:18 +0100

    Improve Button UI - Now indicates what section is selected

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,15 +1,21 @@
 export default function Button({
   label,
   handleClick,
+  categorySelect,
 }: {
   label: string;
   handleClick?: React.MouseEventHandler<HTMLButtonElement>;
+  categorySelect: string;
 }) {
   return (
     <>
       <button
         type="button"
-        className="size-30 bg-amber-100 text-blue-900 border border-blue-900 cursor-pointer"
+        className={`text-base font-light md:text-lg md:size-24 xl:size-30 xl:font-semibold ${
+          label == categorySelect
+            ? "bg-blue-400 text-stone-100"
+            : "bg-stone-100 text-stone-900"
+        }  shadow cursor-pointer rounded`}
         key={label}
         onClick={handleClick}
       >
