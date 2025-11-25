b2011b13ef23cd2b3ae03bba7dc8a0b2db26900d
Fu Jun <fulin.fujun@outlook.es>
2025-11-25 23:39:43 +0100

    Add client info and improve ticket format

--- backend/index.js
+++ backend/index.js
@@ -17,80 +17,83 @@ app.get('/status', async (req, res) => {
     res.json({ printerIsConnected: isConnected, printerInfo: printerInfo.path });
 })
 
-app.post('/example', (req, res) => {
-    const data = req.body;
-    const articlesLines = data.articlesLines;
-    //Header
-    printer.alignCenter();
-    printer.println("Restaurant");
-    printer.bold(true);
-    printer.println("MY RESTAURANT");
-    printer.bold(false);
-    printer.println("C/ Some Street 55");
-    printer.println("Tel: 91 354 25 96");
-    printer.drawLine();
 
-    //Ticket lines
-    printer.alignLeft();
-    printer.println('Articles lines');
-    printer.drawLine();
-    printer.table(['Qn', 'name', 'Price', 'Total'])
-    for (let i = 0; i < articlesLines.length; i++) {
-        const articleLine = articlesLines[i];
-        printer.table([articleLine.quantity, articleLine.name.slice(0, 10), articleLine.price, articleLine.total]);
+
+app.post('/print', async (req, res) => {
+
+    function getDate() {
+        const date = Date.now();
+        const today = new Date(date);
+
+        let day = today.getDate().toString();
+        if (day.length < 2) {
+            day = "0" + day;
+        }
+        let month = (today.getMonth() + 1).toString();
+        if (month.length < 2) {
+            month = "0" + month;
+        }
+        let year = today.getFullYear();
+        let hours = today.getHours();
+        let minutes = today.getMinutes().toString();
+        if (minutes.length < 2) {
+            minutes = "0" + minutes;
+        }
+
+        return `${day}/${month}/${year} ${hours}:${minutes}`;
     }
-    printer.drawLine();
-    //Footer
-    printer.alignCenter();
-    printer.println("Thanks for your visit");
-    printer.newLine();
-    printer.newLine();
-    printer.newLine();
-    console.log(printer.getText());
-    res.json({ ticket: printer.getText() });
-})
-
-app.post('/print', (req, res) => {
     const data = req.body;
     const articlesLines = data.articlesLines;
+    const total = data.totalBill;
+    const today = getDate();
     //Header
     printer.alignCenter();
-    printer.println("Restaurant");
+    printer.println("Cafeteria");
+    await printer.printImage('./assets/iestacio_logo.png');
     printer.bold(true);
-    printer.println("MY RESTAURANT");
+    printer.println();
+    printer.println("L'ESTACIÓ");
     printer.bold(false);
-    printer.println("C/ Some Street 55");
-    printer.println("Tel: 91 354 25 96");
+    printer.println("Ctra. l'estació S/N");
+    printer.println("Tel: 96 291 93 75");
+    printer.println("Email: 46006100@edu.gva.es")
     printer.drawLine();
 
     //Ticket lines
     printer.alignLeft();
-    printer.println('Articles lines');
+    printer.println(today);
     printer.drawLine();
     // printer.table(['Qn', 'name', 'Price', 'Total'])
     printer.tableCustom([
-        { text: 'Cant', align: 'CENTER', cols: 6 },
-        { text: 'Nombre', align: 'CENTER', cols: 23 },
-        { text: 'Pre.', align: 'CENTER', cols: 6 },
+        { text: 'Cant', align: 'CENTER', cols: 7 },
+        { text: 'Nombre', align: 'CENTER', cols: 27 },
+        { text: 'Pre.', align: 'CENTER', cols: 7 },
         { text: 'Total', align: 'CENTER', cols: 7 },
     ]);
     for (let i = 0; i < articlesLines.length; i++) {
         const articleLine = articlesLines[i];
         // printer.table([articleLine.quantity, articleLine.name.slice(0, 10), articleLine.price, articleLine.total]);
         printer.tableCustom([
-            { text: articleLine.quantity, align: 'CENTER', cols: 6 },
-            { text: articleLine.name, align: 'LEFT', cols: 23 },
-            { text: articleLine.price, align: 'CENTER', cols: 6 },
-            { text: articleLine.total, align: 'CENTER', cols: 7 },
+            { text: articleLine.quantity, align: 'CENTER', cols: 7 },
+            { text: ` ${articleLine.name.toUpperCase().slice(0, 25)} `, align: 'LEFT', cols: 27 },
+            {
+                text: `${articleLine.price.toFixed(2).toString().replace('.', ',')}`, align: 'CENTER', cols: 7
+            },
+            { text: `${articleLine.price.toFixed(2).toString().replace('.', ',')}`, align: 'CENTER', cols: 7 },
         ]);
     }
+    printer.drawLine();
+    printer.alignRight();
+    printer.println(`TOTAL: ${total.toFixed(2).toString().replace('.', ',')} €`);
     printer.drawLine();
 
     //Footer
     printer.alignCenter();
-    printer.println("Thanks for your visit");
+    printer.println("Gracies per la seua visita");
+    printer.printQR('https://portal.edu.gva.es/iestacio/', { cellSize: 5 });
     printer.newLine();
     printer.newLine();
+    printer.cut();
 
     try {
         const data = printer.getBuffer();
@@ -141,6 +144,42 @@ app.post('/open-drawer', (req, res) => {
         console.error('Cashdrawer error:', error);
     }
 });
+
+app.post('/example', async (req, res) => {
+    const data = req.body;
+    const articlesLines = data.articlesLines;
+    //Header
+    printer.alignCenter();
+    printer.println("Cafeteria");
+    await printer.printImage('./assets/iestacio_logo.png');
+    printer.bold(true);
+    printer.println("L'ESTACIÓ");
+    printer.bold(false);
+    printer.println("Ctra. l'estció S/N");
+    printer.println("Tel: 96 291 93 75");
+    printer.println("Email: 46006100@edu.gva.es")
+    printer.drawLine();
+
+    //Ticket lines
+    printer.alignLeft();
+    printer.println('Articles lines');
+    printer.drawLine();
+    printer.table(['Qn', 'name', 'Price', 'Total'])
+    for (let i = 0; i < articlesLines.length; i++) {
+        const articleLine = articlesLines[i];
+        printer.table([articleLine.quantity, articleLine.name.slice(0, 23), articleLine.price, articleLine.total]);
+    }
+    printer.drawLine();
+    //Footer
+    printer.alignCenter();
+    printer.println("Gracies per la seua visita");
+    printer.printQR('https://portal.edu.gva.es/iestacio/');
+    printer.newLine();
+    printer.newLine();
+    printer.newLine();
+    console.log(printer.getText());
+    res.json({ ticket: printer.getText() });
+})
 
 app.listen(port, () => {
     console.log(`Print server listening on http://localhost:${port}`);
